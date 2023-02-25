71c14e7c824b41cf86b96da07bd47482836e9aee
s0600204 <dev@s0600204>
2023-02-25 01:20:45 +0000

    Implement basic Audio Cue exporting code

--- interpreters/audio_cue_interpreter.py
+++ interpreters/audio_cue_interpreter.py
@@ -41,20 +41,21 @@ Sub Nodes:
 
         LogicalDev{x}   string      device name
         DBLevel{x}      float       volume
+        Pan{x}          integer     0 -> *500 -> 1000
 
-    Optional in 11.2.3:
-        Pan{x}          integer
+        LvlPtLvlSelA    enum        LvlIndiv | LvlSync | LvlLink
+        LvlPtPanSelA    enum        PanUseAudDev | PanIndiv | PanSync
 
-    Optional in 11.4.0.3 and 11.4.1.1:
-        ## The following exists on every AudioFile subcue @ 11.4.0.3, 11.4.1.1, but not @ 11.2.3
-        LvlPtLvlSelA    ??          ["LvlIndiv"]
-        LvlPtPanSelA    ??          ["PanUseAudDev"]
-        LvlPt           array
-            LvlPtType                   ["FadeIn", "End"]
+        LvlPt           array       A point of the integrated fade envelope
+            LvlPtType                   Start | FadeIn | FadeOut | End
             LvlPtItem       array
                 LvlPtItemLogicalDev     string      device name
                 LvlPtItemRelDBLevel     float       volume
+                LvlPtItemPan            integer     0 -> *500 -> 1000
 """
+
+
+from lisp.backend.audio_utils import linear_to_db
 
 
 class AudioCueInterpreter:
@@ -64,10 +65,42 @@ class AudioCueInterpreter:
     scs_cuetype = "F"
 
     def __init__(self):
-        pass
+        print("Audio cue interpreter init")
 
-    def export_cue(self, lisp_cue):
-        pass
+    def export_cue(self, exporter, lisp_cue):
+        if not hasattr(lisp_cue.media.elements, "UriInput"):
+            # @todo: Warn user that this cue will be skipped before export process
+            return []
+
+        scs_cue = exporter.build_generic_cue(lisp_cue)
+        subcue = exporter.build_generic_subcue(lisp_cue, self.scs_cuetype)
+        details = exporter.dom.createElement("AudioFile")
+
+        # @todo:
+        # * lose the file:/// prefix
+        # * change the slashes from `/` to `\`
+        uri = lisp_cue.media.elements.UriInput.uri
+        details.appendChild(exporter.create_text_element("FileName", uri))
+
+        # @todo:
+        # * Use actual device name
+        details.appendChild(exporter.create_text_element("LogicalDev0", "Front"))
+
+        if hasattr(lisp_cue.media.elements, "Volume"):
+            details.appendChild(
+                exporter.create_text_element(
+                    "DBLevel0", linear_to_db(lisp_cue.media.elements.Volume.volume)))
+
+        if hasattr(lisp_cue.media.elements, "AudioPan"):
+            # LiSP pan: -1.0 <-> 1.0
+            # SCS pan: 0 -> 1000
+            details.appendChild(
+                exporter.create_text_element(
+                    "Pan0", int((lisp_cue.media.elements.AudioPan.pan + 1) * 500)))
+
+        subcue.appendChild(details)
+        scs_cue.appendChild(subcue)
+        return [scs_cue]
 
     def import_cue(self, scs_cue):
         pass
